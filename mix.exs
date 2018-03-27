b998a58045d20162cefc5c433a9a93d7a241e56d
RaymondLoranger <Raymond.Loranger@bell.net>
2018-03-26 20:06:06 -0400

    0.1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MixTasks.Mixfile do
   def project do
     [
       app: :mix_tasks,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
