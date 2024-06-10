f8f380a744f4bec32115f7dca3acc344f1ddd800
RaymondLoranger <Raymond.Loranger@bell.net>
2024-06-10 10:00:13 -0400

    0.3.31

---
+++ lib/mix/tasks/ver.inc.ex
@@ -0,0 +1,28 @@
+defmodule Mix.Tasks.Ver.Inc do
+  @moduledoc "Increments the app version."
+
+  @shortdoc "Increments the app version"
+
+  use Mix.Task
+
+  @doc """
+  Increments the app version.
+
+  ## Examples
+
+      mix ver.inc # => will increment the app version
+  """
+  @impl Mix.Task
+  @spec run(OptionParser.argv()) :: :ok
+  def run(_args) do
+    %Version{} = version = Mix.Project.config()[:version] |> Version.parse!()
+    new_version = update_in(version.patch, &(&1 + 1))
+
+    File.write!(
+      "mix.exs",
+      "mix.exs"
+      |> File.read!()
+      |> String.replace(~s|"#{version}"|, ~s|"#{new_version}"|, global: false)
+    )
+  end
+end
