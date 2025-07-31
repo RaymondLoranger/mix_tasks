cd7cb3f83e2d03235a93b54c08c7c358bf321ebd
RaymondLoranger <Raymond.Loranger@bell.net>
2025-07-31 16:42:36 -0400

    0.3.46

--- lib/mix/tasks/custom.cmd.ex
+++ lib/mix/tasks/custom.cmd.ex
@@ -19,12 +19,34 @@ defmodule Mix.Tasks.Custom.Cmd do
   @spec run(OptionParser.argv()) :: :ok
   def run(~w<git branch> = args) do
     Mix.Tasks.Echo.run(args)
+
+    # Example:
+    # git branch
+    #   elementary-watson
+    # * improve-usage
+    #   main
     [bef, aft] = "#{:os.cmd(~c"git branch")}" |> String.split("*")
+    # => ["  elementary-watson\n", " improve-usage\n  main\n"]
+    # bef = "  elementary-watson\n"
+    # aft = " improve-usage\n  main\n"
+
     [cur, etc] = String.split(aft, "\n", parts: 2)
+    # => [" improve-usage", "  main\n"]
+    # cur = " improve-usage"
+    # etc = "  main\n"
 
     [bef, ["*", :light_green, cur, :reset, "\n"], etc]
+    # => [
+    #      "  elementary-watson\n",
+    #      ["*", :light_green, " improve-usage", :reset, "\n"],
+    #      "  main\n"
+    #    ]
     |> IO.ANSI.format()
     |> IO.write()
+
+    # =>   elementary-watson
+    #    * improve-usage
+    #      main
   end
 
   def run(args) do
